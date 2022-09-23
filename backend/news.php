f58caf91ba4067dd7d3508fd23adab14edb05316
TCHF25 <70825865+TCHF25@users.noreply.github.com>
2022-09-23 23:33:07 +0300

    coding queries

--- backend/news.php
+++ backend/news.php
@@ -3,3 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 
 include("connection.php");
+
+$query = $mysqli->prepare("SELECT * FROM new");
+$query->execute();
+$array = $query->get_result();
